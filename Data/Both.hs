56e4b2d0dec2b843e6941bf7b6f8734ce86399f5
Michael Walker <mike@barrucadu.co.uk>
2015-08-22 12:43:35 +0100

    Initial commit: import from wheat

---
+++ Data/Both.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- | The 'Both' type and operations. Like 'Maybe', but not.
+module Data.Both where
+
+import Control.Applicative
+import Control.Monad
+import Data.Data
+import Data.Foldable
+import Data.Maybe
+import Data.Monoid
+import Data.Traversable
+import GHC.Generics
+
+newtype Both a = Both { getBoth :: Maybe a }
+  deriving (Eq, Ord, Read, Show, Data, Typeable, Generic, Generic1, Functor, Applicative, Alternative, Monad, MonadPlus, Foldable, Traversable)
+
+-- | The 'mappend' for 'Maybe' is 'Just' if /either/ of the operands
+-- are, whereas here /both/ must be.
+instance Monoid a => Monoid (Both a) where
+  mempty = Both $ Just mempty
+  mappend (Both (Just x)) (Both (Just y)) = Both . Just $ x <> y
+  mappend _ _ = Both Nothing
+
+-- | The 'both' function takes a default value, a function, and a
+-- 'Both' value. If the inner 'Maybe' value is 'Nothing', the function
+-- returns the default value. Otherwise, it applies the function to
+-- the value inside the 'Just' and returns the result.
+both :: b -> (a -> b) -> Both a -> b
+both z f = maybe z f . getBoth
+
+-- | The 'fromBoth' function takes a default value and a 'Both'
+-- value. If the inner 'Maybe' is 'Nothing', it returns the default
+-- value; otherwise, it returns the value contained within.
+fromBoth :: a -> Both a -> a
+fromBoth z = fromMaybe z . getBoth
---
+++ README.markdown
@@ -0,0 +1,39 @@
+both [![Build Status][build-status]][build-log]
+====
+
+The Monoid instance for Maybe behaves like so:
+
+~~~~{.haskell}
+instance Monoid a => Monoid (Maybe a) where
+  mappend (Just x) (Just y) = Just $ x <> y
+  mappend (Just x) Nothing  = Just x
+  mappend Nothing  (Just y) = Just y
+  mappend Nothing  Nothing  = Nothing
+
+  mempty = Nothing
+~~~~
+
+Both is a newtype wrapper around Maybe providing this instance:
+
+~~~~{.haskell}
+instance Monoid a => Monoid (Both a) where
+  mappend (Just x) (Just y) = Just $ x <> y
+  mappend _ _ = Nothing
+
+  mempty = Just mempty
+~~~~
+
+The documentation of the latest developmental version is
+[available online][docs].
+
+Contributing
+------------
+
+Bug reports, pull requests, and comments are very welcome!
+
+Feel free to contact me on GitHub, through IRC (#haskell on freenode),
+or email (mike@barrucadu.co.uk).
+
+[build-status]: http://ci.barrucadu.co.uk/job/(both)/job/both/badge/icon?style=plastic
+[build-log]:    http://ci.barrucadu.co.uk/job/(both)/job/both/
+[docs]:         https://barrucadu.github.io/both
